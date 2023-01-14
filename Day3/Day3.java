314fd4c398c206cc6f112972cf8fec843740cc51
Gael <gaelthegoodboy@gmail.com>
2023-01-15 04:02:14 +0800

    Finished Day 3 and its actually pretty fun now heheh

--- Day3/Day3.java
+++ Day3/Day3.java
@@ -5,9 +5,10 @@ import java.util.*;
 
 public class Day3{
     static ArrayList<ArrayList<Integer>> Sum = new ArrayList<>();
-    static ArrayList<ArrayList<String>> GroupSum = new ArrayList<>(); //1 big arraylist
+    static ArrayList<Integer> GroupSum = new ArrayList<>(); //1 big arraylist
     static HashMap <String,Integer> BigValues = new HashMap<>();
     static HashMap <String,Integer> SmallValues = new HashMap<>();
+    static ArrayList<String> Values = new ArrayList<>();
     
     private static void Analyse(String line, HashMap<String,Integer> Big, HashMap<String,Integer> Small){
         String[] arrayn = line.split("");
@@ -40,9 +41,45 @@ public class Day3{
 
     }
 
-    static void Group(String line, HashMap<String,Integer> Big, HashMap<String,Integer> Small){
-        String[] arrayn = line.split("");
-        ArrayList<Integer> common1 = new ArrayList<>();
+    static void Group(String line1, String line2, String line3, HashMap<String,Integer> Big, HashMap<String,Integer> Small){
+        ArrayList <String> Group3 = new ArrayList<>();
+        HashSet<String> NoRep = new HashSet<>();
+        ArrayList <String> Container = new ArrayList<>();
+        String[] one1 = line1.split("");
+        String[] two1 = line2.split("");
+        String[] three1 = line3.split("");
+        Group3.add(line1);
+        Group3.add(line2);
+        Group3.add(line3);
+        for (int i = 0; i < one1.length; i++){
+            NoRep.add(one1[i]);
+        }
+
+        for (int i = 0; i < two1.length; i++){
+            NoRep.add(two1[i]);
+        }
+
+        for (int i = 0; i < three1.length; i++){
+            NoRep.add(three1[i]);
+        }
+
+        ArrayList <String> StringSet = new ArrayList<>(NoRep);
+
+        for (int i = 0; i < NoRep.size(); i++){
+            if (!Container.contains(StringSet.get(i)) && Arrays.asList(one1).contains(StringSet.get(i)) && Arrays.asList(two1).contains(StringSet.get(i)) && Arrays.asList(three1).contains(StringSet.get(i))){
+                Container.add(StringSet.get(i));
+            }
+        }
+
+        if (Big.containsKey(Container.get(0))){
+            GroupSum.add(Big.get(Container.get(0)));
+        }
+
+        else if (Small.containsKey(Container.get(0))){
+            GroupSum.add(Small.get(Container.get(0)));
+        }
+
+        //GroupSum.add(Group3);
 
 
     }
@@ -65,18 +102,7 @@ public class Day3{
                 ArrayList<String> minigroup = new ArrayList<>(); 
                 String line = reader.nextLine();
                 Analyse(line,BigValues,SmallValues);
-                if (count > 3){
-                    count = 1;
-                    if (count == 1){
-                        GroupSum.add(line);
-                        count++;
-                    }
-                }
-                else if (count <= 3){
-                    minigroup.add(line);
-                    count++;
-                }
-
+                Values.add(line);
             }
 
 
@@ -85,11 +111,22 @@ public class Day3{
             for (int j = 0; j < Sum.get(i).size(); j++){
                 max += Sum.get(i).get(j);
             }
-
-            System.out.println(GroupSum);
         }
 
+            for (int i = 2; i < Values.size(); i+=3){
+                Group(Values.get(i),Values.get(i-1),Values.get(i-2), BigValues, SmallValues);
+            }
+
+            int max2 = 0;
+            for (int i=0; i<GroupSum.size(); i++){
+                max2 += GroupSum.get(i);
+            }
+
         System.out.println(max);
+        System.out.println(max2);
+        
+       
+       
         }
 
         catch(FileNotFoundException error){
